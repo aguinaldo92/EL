7b53771617b613ad37c262f97dfa1ac9a23c4da4
aguinaldo92 <aguinaldo.licastro@live.it>
2016-01-08 10:55:36 +0100

    fix warning ";"

--- app/data.js
+++ app/data.js
@@ -6,7 +6,7 @@ app.factory("Data", ['$http', 'toaster',
         var obj = {};
         obj.toast = function (data) {
             toaster.pop(data.status, "", data.message, 10000, 'trustedHtml');
-        }
+        };
         obj.get = function (q) {
             return $http.get(serviceBase + q).then(function (results) {
                 return results.data;
